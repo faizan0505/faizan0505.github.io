de4e04e1597ff53bd7d71a41a019edcc8d0dea2d
unknown <md.faizan9t9@gmail.com>
2023-03-23 13:49:09 +0530

    Portfolio

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ theme.addEventListener("change", () => {
     // alert("hii")
     document.body.classList.toggle("light")
 })
+
+
+let openResume = document.getElementById("resume-button-1");
+    
+    openResume.addEventListener("click", () => {
+    window.open('https://drive.google.com/file/d/1usR-bK7wCMQwGnl-8lMzI2Y--QhHLcec/view?usp=share_link', '_blank'); 
+    
+   })
