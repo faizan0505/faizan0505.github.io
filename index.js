2e26714f061cca89e9da8e7f77f3a94fa751c526
Faizan <faizan@phantasm.co.in>
2024-04-19 00:43:50 +0530

    responsive

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 var typed = new Typed(".autotype", {
-    strings: ["Full Stack Web Developer", "Backend Developer", "MERN Stack Developer"],
-    typeSpeed: 100,
-    backSpeed: 80,
-    loop: true
-})
+  strings: [
+    "Full Stack Web Developer",
+    "Backend Developer",
+    "MERN Stack Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 80,
+  loop: true,
+});
 
-
-let theme = document.querySelector("#theme")
+let theme = document.querySelector("#theme");
 
 theme.addEventListener("change", () => {
-    // alert("hii")
-    document.body.classList.toggle("light")
-})
-
+  // alert("hii")
+  document.body.classList.toggle("light");
+});
 
 let openResume = document.getElementById("resume-button-1");
-    
-    openResume.addEventListener("click", () => {
-    window.open('https://drive.google.com/file/d/1usR-bK7wCMQwGnl-8lMzI2Y--QhHLcec/view?usp=share_link', '_blank'); 
-    
-   })
+
+openResume.addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1usR-bK7wCMQwGnl-8lMzI2Y--QhHLcec/view?usp=share_link",
+    "_blank"
+  );
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const navMenu = document.getElementById("nav-menu");
+
+  // Toggle the visibility of the menu when the menu icon is clicked
+  menuIcon.addEventListener("click", function () {
+    navMenu.classList.toggle("show");
+  });
+});
